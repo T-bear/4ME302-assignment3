32f797229b41686e0d8c8cd08704c99f26e31e8b
torbjorn.holgersson87@gmail.com <torbjorn.holgersson87@gmail.com>
2018-10-26 08:14:19 +0000

    added correct route to Test_Session

--- twitter-login-node/ey.js
+++ twitter-login-node/ey.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
+ 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-
+ 
 // connection configurations
 const mc = mysql.createConnection({
     host: 'localhost',
@@ -21,23 +21,23 @@ const mc = mysql.createConnection({
     password: '',
     database: 'pd_db'
 });
-
+ 
 // connect to database
 mc.connect();
-
+ 
 // default route
 app.get('/', function (req, res) {
     return res.send({ error: true, message: 'hello' })
 });
-
-// Retrieve all todos
-app.get('/Test_Sessions', function (req, res) {
+ 
+// Retrieve all todos 
+app.get('/test_sessions', function (req, res) {
     mc.query('SELECT * FROM Test_Session', function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'Test_Session' });
     });
 });
-
+ 
 // Search for todos with ‘bug’ in their name
 app.get('/todos/search/:keyword', function (req, res) {
     let keyword = req.params.keyword;
@@ -46,81 +46,72 @@ app.get('/todos/search/:keyword', function (req, res) {
         return res.send({ error: false, data: results, message: 'Todos search list.' });
     });
 });
-
-// Retrieve todo with id
+ 
+// Retrieve todo with id 
 app.get('/todo/:id', function (req, res) {
-
+ 
     let task_id = req.params.id;
-
+ 
     mc.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results[0], message: 'Todos list.' });
     });
-    // Retrieve todo with id
-app.get('/todo/:id', function (req, res) {
-
-    let task_id = req.params.id;
-
-    mc.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'Todos list.' });
-    });
-
+ 
 });
-
-// Add a new todo
+ 
+// Add a new todo  
 app.post('/todo', function (req, res) {
-
+ 
     let task = req.body.task;
-
+ 
     if (!task) {
         return res.status(400).send({ error:true, message: 'Please provide task' });
     }
-
+ 
     mc.query("INSERT INTO tasks SET ? ", { task: task }, function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
     });
 });
-
+ 
 //  Update todo with id
 app.put('/todo', function (req, res) {
-
+ 
     let task_id = req.body.task_id;
     let task = req.body.task;
-
+ 
     if (!task_id || !task) {
         return res.status(400).send({ error: task, message: 'Please provide task and task_id' });
     }
-
+ 
     mc.query("UPDATE tasks SET task = ? WHERE id = ?", [task, task_id], function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
-            });
+    });
 });
-
+ 
 //  Delete todo
 app.delete('/todo/:id', function (req, res) {
-
+ 
     let task_id = req.params.id;
-
+ 
     mc.query('DELETE FROM tasks WHERE id = ?', [task_id], function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
     });
-
+ 
 });
-
+ 
 // all other requests redirect to 404
 app.all("*", function (req, res, next) {
     return res.send('page not found');
     next();
 });
-
+ 
 // port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
 app.listen(80, function () {
-    console.log('Node app is running on port 8080');
+    console.log('Node app is running on port 80');
 });
-
+ 
 // allows "grunt dev" to create a development server with livereload
 module.exports = app;
