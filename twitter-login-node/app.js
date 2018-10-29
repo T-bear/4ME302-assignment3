4abe9ba93b188e98bca0eb2a3542df392264c217
T-bear <torbjorn.holgersson87@gmail.com>
2018-10-29 14:04:17 +0100

    implemented ey.js to app.js

--- twitter-login-node/app.js
+++ twitter-login-node/app.js
@@ -114,6 +114,27 @@ app.use(session({ secret: 'keyboard cat', key: 'sid', resave: false, saveUniniti
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); //here
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pd_db'
+});
+ 
+// connect to database
+mc.connect();
+//-----------------------------------------------------------------------------------------------
+
 
 app.get('/', function(req, res){
   res.render('index', { user: req.user });
@@ -177,5 +198,93 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/login')
 }
 
-app.listen(80);
-console.log('server up');
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+ 
+// Retrieve all todos 
+app.get('/test_sessions', function (req, res) {
+    mc.query('SELECT * FROM Test_Session', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Test_Session' });
+    });
+});
+ 
+// Search for todos with ‘bug’ in their name
+app.get('/todos/search/:keyword', function (req, res) {
+    let keyword = req.params.keyword;
+    mc.query("SELECT * FROM tasks WHERE task LIKE ? ", ['%' + keyword + '%'], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Todos search list.' });
+    });
+});
+ 
+// Retrieve todo with id 
+app.get('/todo/:id', function (req, res) {
+ 
+    let task_id = req.params.id;
+ 
+    mc.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'Todos list.' });
+    });
+ 
+});
+ 
+// Add a new todo  
+app.post('/todo', function (req, res) {
+ 
+    let task = req.body.task;
+ 
+    if (!task) {
+        return res.status(400).send({ error:true, message: 'Please provide task' });
+    }
+ 
+    mc.query("INSERT INTO tasks SET ? ", { task: task }, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
+    });
+});
+ 
+//  Update todo with id
+app.put('/todo', function (req, res) {
+ 
+    let task_id = req.body.task_id;
+    let task = req.body.task;
+ 
+    if (!task_id || !task) {
+        return res.status(400).send({ error: task, message: 'Please provide task and task_id' });
+    }
+ 
+    mc.query("UPDATE tasks SET task = ? WHERE id = ?", [task, task_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+    });
+});
+ 
+//  Delete todo
+app.delete('/todo/:id', function (req, res) {
+ 
+    let task_id = req.params.id;
+ 
+    mc.query('DELETE FROM tasks WHERE id = ?', [task_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+    });
+ 
+});
+ 
+// all other requests redirect to 404
+app.all("*", function (req, res, next) {
+    return res.send('page not found');
+    next();
+});
+ 
+// port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
+app.listen(80, function () {
+    console.log('Node app is running on port 80');
+});
+ 
+// allows "grunt dev" to create a development server with livereload
+module.exports = app;
